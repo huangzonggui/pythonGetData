56ba855d1f01668295f76a0b8d2c988d46812bcf
huangzonggui <1226620085@qq.com>
2018-09-01 14:58:50 +0800

    抓取Tencent new

--- newTen.py
+++ newTen.py
@@ -39,72 +39,45 @@ def wirteToDoc():
     '''
     向后台发起请求，把需要的内容写入docx文档中
     '''
-    i=1
-    while i <= 14:
-        print(i)
+    page=1
+    while page <= 14:
+        #print(page)
         time.sleep(0.5)
         headers = {#为什么将这个头放在while外面不行？？？第二个以后的请求都是Access defined
             "Referer" : "http://" + host,   #需要添加Referer头部，否则请求失败
             "Accept-Encoding": ""
         }
         req = request.Request(
-            "http://" + host + "/interface/roll.php?0.7895971873270728&cata=&site=finance&date=&page="+str(i)+"&mode=1&of=json",
+            "http://" + host + "/interface/roll.php?0.7895971873270728&cata=&site=finance&date=&page="+str(page)+"&mode=1&of=json",
                               headers = headers
         )
         response = request.urlopen(req)
         jsonData = response.read().decode("gbk")
-        jsonData = jsonData.replace('\\', '')
-        #print(jsonData)
-        ret = re.findall(r"http://.+?\.htm", jsonData)
-                        #http://finance.qq.com/a/20180223/025599.htm
-        #print(ret)#ret是新闻链接URL list
-        headers = {
-            "Accept-Encoding": ""
-        }
-        for href in ret:
-            req = request.Request(
-                href,
-                headers=headers
-            )
-            response = request.urlopen(req)#请求数据后的响应
-            #print(str(response))
-            #TODO:存在某些页面无法解码，原因不明，暂时忽略
-            html = response.read().decode("gb2312", "ignore")
-            bsObj = BeautifulSoup(html, "html.parser")
-            page = bsObj.find(
-                "div",
-                {"class": "qq_article"}
-            )#一页全部内容
-            if page:#在一页里面拿我想要的
-                title = page.find("h1")
-                timeStr = page.find(
-                    "span",
-                    {"class": "a_time"}
-                )
-                titdd = page.find(#摘要
-                    "p",
-                    {"class": "titdd-Article"}
-                )
+        #bsObj = BeautifulSoup(jsonData, "html.parser")
+        print(jsonData)
+        #jsonData = bsObj.find(
+         #   "a"
+        #)#一页全部内容
+        #if jsonData:#在一页里面拿我想要的
+            #print(jsonData)
+        
+        ret = re.findall(r".htm\\\">.*?<\\/a>", str(jsonData))#?非贪婪匹配
+        times = re.findall(r"t-time\\\">.*?<\\/span>", str(jsonData))
 
-                run = text.add_run('\n'+str(title.string))
-                #paragraph_format.line_spacing = Pt(18)
-                #run = paragraph.add_run(str(timeStr.string)[6:16])#段落后add_run不换行
-                run.font.size = Pt(11)
-                run.font.bold= True#加粗
+        i = 0
+        for item in ret: 
+            run = text.add_run('\n'+ item[7:-5] + '  ')
+            print (item[7:-5])
+            run.font.bold = True#加粗
 
-                run = text.add_run(str(timeStr.string)[6:16])#时间
-                run.font.size = Pt(9)
-                run.italic = True#斜体
-
-                if titdd!=None:
-                    run = text.add_run('\n'+str(titdd.get_text()))#摘要
-                    run.font.size = Pt(9)
-                    #doc.add_paragraph('')
-                    #doc.add_paragraph(str(title) + str(timeStr) + str(titdd))
-        i = i + 1
+            run = text.add_run(times[i][9:-8])
+            print (times[i][9:-8])
+            i=i+1
+            
+        page = page + 1
+    return ret
 
 if __name__ == "__main__":
     wirteToDoc()#把标题时间抽取出来放在doc中
-    #word  
     saveFile=os.getcwd()+"\\腾讯财经"+localtime+".docx"  
     doc.save(saveFile)#根据saveFile的路径和文件名保存文件
--- runAll.bat
+++ runAll.bat
@@ -9,8 +9,3 @@ start cmd /k "color 1F&&python newSina.py"
 start cmd /k "color 1F&&python newTen.py"
 
 start cmd /k "color 2F&&python NBA.py"
-
-#cd ../secondMethod
-#start cmd /k "color 1F&&python ten1-5.py"
-#start cmd /k "color 1F&&python ten6-9.py"
-#start cmd /k "color 1F&&python ten10-13.py"
